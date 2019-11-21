161acfb4afb7d2fbc577cdf0ff2ad619a4ceb357
nobbbcu <nobbcu@gmail.com>
2019-11-21 17:59:08 +0900

    updated application.rb, users.js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,8 +2,8 @@ $(function(){
   function buildHTML(user){
     var html = `
               <div class="chat-group-user clearfix">
-                <p class="chat-group-user__name">"${user.name}"</p>
-                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                <p class="chat-group-user__name">${user.name}</p>
+                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name=${user.name}>追加</div>
               </div>`
     $('#user-search-result').append(html);
   }
